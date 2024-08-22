479566648046e7c73c63f63983c655bbd2b79cd5
SirojiddinCoder <sirojiddincoder@gmail.com>
2024-08-22 05:37:40 -0700

    input value

--- src/State.jsx
+++ src/State.jsx
@@ -6,12 +6,39 @@ class State extends React.Component{
 
         this.state = {
             count:0,
+            title:"salomlar"
         }
     }
 
     render(){
+
+ const plus =()=>{
+    
+    this.setState({count:this.state.count + 1})
+ }
+
+ const minus =()=>{
+    this.setState({count:this.state.count - 1})
+  
+ }
+
+ const Onchange =(event)=>{ 
+    console.log(event.target.value);
+    this.setState({title:event.target.value})
+    
+ }
+
         return(
-            <div>Count: {this.state.count}</div>
+            <div>
+            <h3>Count: {this.state.count}</h3>
+                <button onClick={plus}>+</button>
+                <button onClick={minus}>-</button>
+                <br />
+                <h4>Tanlash uchun</h4>
+                <h1>{this.state.title}</h1>
+                <input onChange={Onchange} type="text" placeholder="test...." />
+
+            </div>
         )
     }
 }
