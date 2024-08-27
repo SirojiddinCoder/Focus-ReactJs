05d0f073f5c4fbf1c960c9b0100fb92460632f5a
SirojiddinCoder <sirojiddincoder@gmail.com>
2024-08-27 02:37:08 -0700

    useState hook

--- src/Hook.jsx
+++ src/Hook.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Hook = () => {
+
+  const [count,setCount] = useState(0)
   return (
     <div>
         <h1>Hook Components</h1>
+
+        <h2>Count:{count}</h2>
+        <button onClick={()=>setCount(count - 1)}>-</button>
+        <button onClick={()=>setCount(count  + 1)}>+</button>
     </div>
   )
 }
