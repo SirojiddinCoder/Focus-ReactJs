4032fc34267414fcce2255d30f7e9169666edb0c
SirojiddinCoder <sirojiddincoder@gmail.com>
2024-08-27 05:26:05 -0700

    react Lifecycyle

--- src/UseEfect1.jsx
+++ src/UseEfect1.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const UseEfect1 = () => {
+
+    const [count,setCount] = useState(0)
+ 
+      useEffect(()=>{
+        console.log("case 1");
+        
+      })
+      useEffect(()=>{
+        console.log("case 2 ");
+        
+      },[])
+
+      useEffect(()=>{
+        console.log("case 3 ");
+        
+      },[count])
+
   return (
-    <div>UseEfect1</div>
+    <div>
+        <h1>UseEffect</h1>
+
+         <h2>Count:{count}</h2>
+         <button onClick={()=>setCount(count - 1)}>-</button>
+         <button onClick={()=>setCount(count  + 1)}>+</button>
+    </div>
   )
 }
