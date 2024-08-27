5a250ddf524dd51734781157aafccda123eb73f9
SirojiddinCoder <sirojiddincoder@gmail.com>
2024-08-27 02:32:43 -0700

    class state

--- src/Class.jsx
+++ src/Class.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 
 class Class1 extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            count:0,
+        }
+    }
     render(){
         return(
             <div>
                 <h1>Class Components</h1>
+                <h3>Count:{this.state.count}</h3>
+                <button onClick={()=>this.setState({count:this.state.count - 1})}>-</button>
+                <button onClick={()=>this.setState({count:this.state.count + 1})}>+</button>
+                
             </div>
         )
     }
